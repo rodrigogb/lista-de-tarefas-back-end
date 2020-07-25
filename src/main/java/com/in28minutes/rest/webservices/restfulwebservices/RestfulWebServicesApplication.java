071cfbd9f38d9f46d6a39a1e0fc902f0e938a339
Rodrigo Buzato <rodrigo.buzato@ventron.com.br>
2020-07-25 15:49:45 -0300

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		
+		System.out.println("Print no eclipse ide eu sou javeiro d+++");
+	}
+
+}
