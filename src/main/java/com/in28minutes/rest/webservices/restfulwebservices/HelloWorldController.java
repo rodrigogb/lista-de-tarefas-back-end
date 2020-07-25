dc9760b0697e49226e33f913f7bf086a29bd4803
Rodrigo Buzato <rodrigo.buzato@ventron.com.br>
2020-07-25 17:25:55 -0300

    Criacao de um servico que recebe um parametro

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
